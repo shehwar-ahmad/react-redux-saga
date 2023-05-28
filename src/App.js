57d5fe6366ee011374cb69c0d2c296edf2945c14
shehwar <shehwar.dev@gmail.com>
2023-05-28 23:16:19 +0500

    feat: basic redux toolkit setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Counter from "components/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Saga</h1>
+        <Counter />
       </header>
     </div>
   );
