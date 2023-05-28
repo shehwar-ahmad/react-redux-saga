a658763cfae10b917f36b7ad000b544ed2835973
shehwar <shehwar.dev@gmail.com>
2023-05-29 01:36:25 +0500

    feat: saga middleware added

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "redux/reducers/Counter";
+import createSagaMiddleware from "redux-saga";
+
+const saga = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: { counterReducer },
+  middleware: [saga],
 });
